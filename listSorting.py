8fc5e229c9cf047e4edff465800e1076de3146a9
Ross Fadely <rossfadely@gmail.com>
2016-02-17 11:05:54 -0500

    class i/o ok

--- listSorting.py
+++ listSorting.py
@@ -3,10 +3,42 @@ import os
 
 class SortList(object):
     """
-
+    Class to solve week 6 code challenge.
     """
-    def __init__(self, filepath):
+    def __init__(self, input_path, output_path):
+        """
+        Take in string to input file, read it in, and sort if needed.
+        """
+        # open the file, get the line.
+        line = self._open(input_path)
+
+        # if line is empty write to file, else sort.
+        if len(line) == 0:
+            self.result = ''
+        else:
+            self.sort_list(line)
+
+        self._write(output_path)
+
+    def sort_list(self, line):
+        """
+        Sort the line.
+        """
         pass
+
+    def _open(self, filepath):
+        """
+        Read in the text line from file.
+        """
+        return open(filepath).readlines()
+
+    def _write(self, filepath):
+        """
+        Write the text resulting line to file.
+        """
+        f = open(filepath, 'w')
+        f.write(self.result)
+        f.close()
 
 def check_args(args):
     """
@@ -19,6 +51,7 @@ def check_args(args):
     assert len(args) == 3, m
 
     # check that input text file exists
+    assert args[2].split('/')[-1] == 'result.txt'
     assert os.path.isfile(args[1]), 'Input file not found.'
 
     # check output text path is valid
@@ -37,3 +70,5 @@ if __name__ == '__main__':
     # read command line
     import sys
     input_path, output_path = check_args(sys.argv)
+
+    sorter = SortList(input_path, output_path)
